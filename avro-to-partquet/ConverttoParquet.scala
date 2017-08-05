c63a5881b3e9ffcbef20a19dd87163d6899a004b
Dauzy <daniel_6287@hotmail.com>
2017-08-04 23:01:36 -0500

    Se modifico scipt de scala

--- avro-to-partquet/ConverttoParquet.scala
+++ avro-to-partquet/ConverttoParquet.scala
@@ -2,9 +2,9 @@ import com.databricks.spark.avro._
 import org.apache.spark.sql.SQLContext
 
 val sqlContext = new SQLContext(sc)
-val avroDF = sqlContext.read.avro("src/test/be-datio/part-r-00000-d7bcd31a-c121-4809-bfa2-617b1e0253ee.avro")
-avroDF.write.parquet("src/test/parquet/out/archivo.parquet")
-val parqfile = sqlContext.read.parquet("src/test/parquet/out/archivo.parquet")
+val avroDF = sqlContext.read.avro("hdfs://carlos:9000/raw/file.avro")
+avroDF.write.parquet("hdfs://carlos:9000/master/archivo.parquet")
+val parqfile = sqlContext.read.parquet("hdfs://carlos:9000/master/archivo.parquet")
 parqfile.registerTempTable("records")
 val allrecords = sqlContext.sql("SELECT * FROM records")
 println("Comprobando Esquema: \n")
